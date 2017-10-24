336e7b405d9f8f16a9540376367d126ec0a1522b
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-24 15:42:07 -0700

    Finishes the carousel images being displayed on screen and cycles at a set interval. #1

--- public/main.js
+++ public/main.js
@@ -4,4 +4,20 @@ const imageFetch = () => {
   return fetch('/carousel/images').then(res => res.json())
 }
 
-const images = imageFetch()
+const carousel = {
+  slides: null,
+  current: 0
+}
+
+imageFetch().then(documents => (carousel.slides = documents)
+
+setInterval(function() {
+  document
+    .querySelector('.image')
+    .setAttribute('src', `${carousel.slides[carousel.current].url}`)
+  if (carousel.current < carousel.slides.length - 1) {
+    carousel.current++
+  } else {
+    carousel.current = 0
+  }
+}, 4000)
